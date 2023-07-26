3d9828d792c59a7d825e1ae9dd1d2d8ee0ff3b4d
Dmitri Camenev <dmit.cameneff@yandex.ru>
2023-07-26 22:23:16 +0300

    add set_capitalize_string() to project, modify docstring set_upper_string()

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def set_upper_string(input_string: str) -> str:
+    """Возвращает строку со ВСЕМИ заглавными буквами."""
     return input_string.upper()
+
+
+def set_capitalize_string(input_string: str) -> str:
+    """Возвращает строку с первыми заглавными буквами слов."""
+    return input_string.capitalize()
